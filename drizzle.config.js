a16d09fde448c38daefe51ebc0d7fed387b5b548
Arnab Majumder <arnabmajumdar399@gmail.com>
2025-01-31 18:44:23 +0530

    added

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,8 +3,8 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://accounts:ypvN2ke3PGFE@ep-weathered-heart-a58wmzem.us-east-2.aws.neon.tech/AI-Content-Genrator?sslmode=require',
-
+      // url: 'postgresql://accounts:ypvN2ke3PGFE@ep-weathered-heart-a58wmzem.us-east-2.aws.neon.tech/AI-Content-Genrator?sslmode=require',
+      url: 'postgresql://neondb_owner:npg_fPtCMR5jzD0n@ep-lively-tree-a8b2lhle-pooler.eastus2.azure.neon.tech/neondb?sslmode=require'
     }
   };
   
